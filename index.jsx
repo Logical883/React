76bb2e0fd33d98cff428969093dd25c00022bdec
Agyapong Albert Yeboah <125793943+Logical883@users.noreply.github.com>
2025-03-11 12:24:30 +0000

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,20 @@
+// Creating a component 
+
+function Welcome() {
+  return "Hello World"; // Return a string
+}
+
+const welcome = () => {
+    return "Hello World"; // Return a string
+}
+
+const Welcome = () => {
+    return <h1>Hello Logical</h1>; // Return a JSX
+}
+
+
+// Rendering Components
+
+const MyComponent = () => {
+    return <h1>Hello World</h1>;
+};
